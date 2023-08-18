84b00a2288ea9b9f395c118bd43ff27eaaf29d54
Dave Conroy <dave@tiredofit.ca>
2023-08-18 12:29:16 -0700

    sops - remove defaults and old test entries

--- features/cli/sops.nix
+++ features/cli/sops.nix
@@ -1,6 +1,5 @@
 { config, inputs, pkgs, ...}:
 {
-
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
@@ -17,18 +16,6 @@
       ];
   };
 
-  systemd.user.services.sops-nix.Unit.After = [ "sops-nix.service" ];
-
-  sops.defaultSopsFile = ../../secrets/example.yaml;
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-  sops.secrets.example_key = {};
-  sops.secrets.flake = {
-    format = "binary";
-    sopsFile = ../../secrets/flake.yaml;
-  };
-
-  #sops.secrets.remotehosts = {
-  #  format = "binary";
-  #  sopsFile = ../../secrets/remotehosts;
-  #};
+  systemd.user.services.sops-nix.Unit.After = [ "sops-nix.service" ];
 }
