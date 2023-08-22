53993b359b2cea581e479b44837e7ee47446e51a
Dave Conroy <dave@tiredofit.ca>
2023-08-22 09:55:39 -0700

    org: generic - init

---
+++ home/generic/default.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, specialArgs, ...}:
+let
+  s = "ti";
+  _p = "re";
+  _a = "do";
+  m = "f";
+  t = "i";
+  r = "t";
+  a_ = ".";
+  p_ = "ca";
+  username = "dave";
+  email = "${username}@${s}${_p}${_a}${m}${t}${r}${a_}${p_}";
+
+  inherit (specialArgs) hostname role;
+  inherit (pkgs.stdenv) isLinux isDarwin;
+  homeDir = if isDarwin then "/Users/" else "/home/";
+  if-exists = f: builtins.pathExists f;
+  existing-imports = imports: builtins.filter if-exists imports;
+in
+{
+  imports = [
+
+  ] ++ existing-imports [
+    ./${hostname}
+    ./${hostname}.nix
+    ./${role}
+    ./${role}.nix
+  ];
+
+  home = {
+    homeDirectory = homeDir+username;
+    inherit username;
+
+    packages = with pkgs;
+    [
+
+    ]
+    ++ lib.optionals ( role == "workstation" || role == "server" )
+    [
+
+    ];
+  };
+
+  programs = {
+    git = {
+      userEmail = email;
+    };
+  };
+}
