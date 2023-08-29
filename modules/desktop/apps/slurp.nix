9d992a2730665530b30ef0d183495ff120374daf
Dave Conroy <dave@tiredofit.ca>
2023-08-29 16:54:53 -0700

    modules - add host.home.applications.slurp

---
+++ modules/desktop/apps/slurp.nix
@@ -0,0 +1,27 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.home.applications.slurp;
+in
+  with lib;
+{
+  options = {
+    host.home.applications.slurp = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Wayland screenshot tool";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      packages = with pkgs;
+        [
+          slurp
+        ];
+    };
+
+  };
+}
