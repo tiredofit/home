03ccaa9e84575ef96280bbfa37519042feb734f4
Dave Conroy <dave@tiredofit.ca>
2023-08-29 10:24:34 -0700

    modules - add host.home.applications.dust

---
+++ modules/applications/cli/dust.nix
@@ -0,0 +1,33 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.home.applications.dust;
+in
+  with lib;
+{
+  options = {
+    host.home.applications.dust = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "'du' command line replacement";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      packages = with pkgs;
+        [
+          dust
+        ];
+    };
+    programs = {
+      bash = {
+        shellAliases = {
+          du = "dust" ;    # disk usage alternative
+        };
+      };
+    };
+  };
+}
