4dd4bd6b5f8647222515454eb0fa40670d7166a7
Dave Conroy <dave@tiredofit.ca>
2023-08-30 09:54:53 -0700

    nix - add logfile to $HOME/.local/state/home-manager/logs on generation switch

--- home/common/nix.nix
+++ home/common/nix.nix
@@ -5,7 +5,9 @@ with lib;
     activation = {
       report-changes = ''
         PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
+        mkdir -p $HOME/.local/state/home-manager/logs
         ${pkgs.nvd}/bin/nvd diff $(ls -dv /nix/var/nix/profiles/per-user/$USER/profile-*-link | tail -2)
+        nvd diff $(ls -dv /nix/var/nix/profiles/per-user/$USER/profile-*-link | tail -2) > "$HOME/.local/state/home-manager/logs/$(date +'%Y%m%d%H%M%S')-$USER-$(ls -dv /nix/var/nix/profiles/per-user/$USER/profile-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv /nix/var/nix/profiles/per-user/$USER/profile-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log"
       '';
     };
   };
