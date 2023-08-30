7a20464f74a600512f81edef043ea452fd2bd9c6
Dave Conroy <dave@tiredofit.ca>
2023-08-30 10:04:25 -0700

    home-manager - Add activation check to create a series of $HOME subdirectories

--- home/common/home-manager.nix
+++ home/common/home-manager.nix
@@ -2,6 +2,25 @@
 with lib;
 {
   home = {
+    activation = {
+      profile_directories_state_create = ''
+        if [ -d "$HOME"/.cache ]; then
+            mkdir -p "$HOME"/.cache
+        fi
+
+        if [ -d "$HOME"/.local/share ]; then
+            mkdir -p "$HOME"/.local/share
+        fi
+
+        if [ -d "$HOME"/.local/state ]; then
+            mkdir -p "$HOME"/.local/state
+        fi
+
+        if [ -d "$HOME"/.config ]; then
+            mkdir -p "$HOME"/.config
+        fi
+      '';
+    };
     stateVersion = mkDefault "23.11";
   };
 
