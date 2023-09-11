b43f04cfd4ce297354ce7efde9c57ac0794d2e66
Dave Conroy <dave@tiredofit.ca>
2023-09-11 09:04:02 -0700

    dotfiles/sway - remove erroneous script directory

--- dotfiles/sway/scripts/x_3screenlayout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-xrandr \
-        --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal \
-        --output DP-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal \
-        --output HDMI-1 --mode 2560x1440 --pos 5120x0 --rotate normal \
-        --output DP-1 --off \
