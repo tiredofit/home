6cc5161185e07cacf76dfb723069c49c617fabc6
Dave Conroy <dave@tiredofit.ca>
2023-11-29 08:30:52 -0800

    modules/hardware/sound - add default sink cycle script

--- modules/desktop/apps/i3status-rs.nix
+++ modules/desktop/apps/i3status-rs.nix
@@ -95,8 +95,8 @@ in
                     driver = "pulseaudio";
                     format = " $icon { $volume|} ";
                     mappings = {
-                      "alsa_output.pci-0000_2d_00.4.analog-stereo" = "";
-                      "alsa_output.usb-0b0e_Jabra_SPEAK_510_USB_745C4BA487C2021900-00.analog-stereo" = "";
+                      "alsa_output.usb-0b0e_Jabra_SPEAK_510_USB_745C4BA487C2021900-00.analog-stereo" = "";
+                      "alsa_output.pci-0000_10_00.6.analog-stereo.2" = "";
                     };
                     #"bluez_output.00_00_00_00_00_00.1" = "";
                     step_width = 1;
