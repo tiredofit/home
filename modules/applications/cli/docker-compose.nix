92ae78fd0b94083cd99d5d2d1f2435cc2f9f12b1
Dave Conroy <dave@tiredofit.ca>
2023-12-04 10:56:42 -0800

    modules/applications/cli/docker-compose - update DOCKER_CONFIG declaration

--- modules/applications/cli/docker-compose.nix
+++ modules/applications/cli/docker-compose.nix
@@ -23,9 +23,9 @@ with lib;
           ### Docker
 
           if [ -n "$XDG_CONFIG_HOME" ] ; then
-              export DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker"
+              export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
           else
-              export DOCKER_CONFIG = "$HOME/.config/docker"
+              export DOCKER_CONFIG="$HOME/.config/docker"
           fi
 
           export DOCKER_TIMEOUT=$DOCKER_TIMEOUT:-"120"
