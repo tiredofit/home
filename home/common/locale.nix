d3436f1e89bd243562cffcd06641cb0c70aec60f
Dave Conroy <dave@tiredofit.ca>
2023-12-11 15:13:35 -0800

    hosts/common/locale - mkDefault all values

--- home/common/locale.nix
+++ home/common/locale.nix
@@ -2,17 +2,17 @@
 
   home = {
     language = {
-      base = "en_US.UTF-8";
-      address = "en_US.UTF-8";
-      collate = "en_US.UTF-8";
-      ctype = "en_US.UTF-8";
-      measurement ="en_US.UTF-8";
-      messages ="en_US.UTF-8";
-      monetary ="en_US.UTF-8";
-      name = "en_US.UTF-8";
-      numeric ="en_US.UTF-8";
-      paper = "en_US.UTF-8";
-      time = "en_US.UTF-8";
+      base = mkDefault "en_US.UTF-8";
+      address = mkDefault "en_US.UTF-8";
+      collate = mkDefault "en_US.UTF-8";
+      ctype = mkDefault "en_US.UTF-8";
+      measurement = mkDefault "en_US.UTF-8";
+      messages = mkDefault "en_US.UTF-8";
+      monetary = mkDefault "en_US.UTF-8";
+      name = mkDefault "en_US.UTF-8";
+      numeric = mkDefault "en_US.UTF-8";
+      paper = mkDefault "en_US.UTF-8";
+      time = mkDefault "en_US.UTF-8";
     };
   };
 }
