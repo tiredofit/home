d6528ff8d0263d9ec2ba8fc15f5e4516d13d4a54
Dave Conroy <dave@tiredofit.ca>
2023-12-11 14:35:41 -0800

    modules/applications/firefox - implement mime defaults within module

--- modules/applications/gui/firefox.nix
+++ modules/applications/gui/firefox.nix
@@ -11,6 +11,31 @@ in with lib; {
         type = with types; bool;
         description = "Web Browser";
       };
+      defaultApplication = {
+        enable = mkOption {
+          description = "MIME default application configuration";
+          type = with types; bool;
+          default = false;
+        };
+        mimeTypes = mkOption {
+          description = "MIME types to be the default application for";
+          type = types.listOf types.str;
+          default = [
+            "application/x-extension-htm"
+            "application/x-extension-html"
+            "application/x-extension-shtml"
+            "application/x-extension-xht"
+            "application/x-extension-xhtml"
+            "application/xhtml+xml"
+            "text/html"
+            "x-scheme-handler/about"
+            "x-scheme-handler/chrome"
+            "x-scheme-handler/http"
+            "x-scheme-handler/https"
+            "x-scheme-handler/unknown"
+          ];
+        };
+      };
     };
   };
 
@@ -3256,5 +3281,8 @@ in with lib; {
         };
       };
     };
+    xdg.mimeApps.defaultApplications = mkIf cfg.defaultApplication.enable (
+      lib.genAttrs cfg.defaultApplication.mimeTypes (_: "firefox.desktop")
+    );
   };
 }
