2e819ec0ceb3048f2a3f07db6da671d02acc7761
Dave Conroy <dave@tiredofit.ca>
2023-12-11 14:36:05 -0800

    modules/applications/gui/eog - implement mime defaults within module

--- modules/applications/gui/eog.nix
+++ modules/applications/gui/eog.nix
@@ -12,6 +12,41 @@ in
         type = with types; bool;
         description = "Gnome Image viewer";
       };
+      defaultApplication = {
+        enable = mkOption {
+          description = "MIME default application configuration";
+          type = with types; bool;
+          default = false;
+        };
+        mimeTypes = mkOption {
+          description = "MIME types to be the default application for";
+          type = types.listOf types.str;
+          default = [
+            "image/bmp";
+            "image/gif";
+            "image/jpeg";
+            "image/jpg";
+            "image/pjpeg";
+            "image/png";
+            "image/tiff";
+            "image/x-bmp";
+            "image/x-gray";
+            "image/x-icb";
+            "image/x-ico";
+            "image/x-png";
+            "image/x-portable-anymap";
+            "image/x-portable-bitmap";
+            "image/x-portable-graymap";
+            "image/x-portable-pixmap";
+            "image/x-xbitmap";
+            "image/x-xpixmap";
+            "image/x-pcx";
+            "image/svg+xml";
+            "image/svg+xml-compressed";
+            "image/vnd.wap.wbmp;image/x-icns";
+          ];
+        };
+      };
     };
   };
 
@@ -22,5 +57,9 @@ in
           gnome.eog
         ];
     };
+
+    xdg.mimeApps.defaultApplications = mkIf cfg.defaultApplication.enable (
+      lib.genAttrs cfg.defaultApplication.mimeTypes (_: "org.gnome.eog.desktop")
+    );
   };
 }
