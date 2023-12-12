f9ad85a6346b8cdf8589d5e78c87ce7c179416bd
Dave Conroy <dave@tiredofit.ca>
2023-12-12 15:34:11 -0800

    modules/applications/cli/zathura - set mime defaults

--- modules/applications/cli/zathura.nix
+++ modules/applications/cli/zathura.nix
@@ -12,6 +12,52 @@ in
         type = with types; bool;
         description = "Console PDF viewer";
       };
+      defaultApplication = {
+        enable = mkOption {
+          description = "MIME default application configuration";
+          type = with types; bool;
+          default = false;
+        };
+        mimeTypes = mkOption {
+          description = "MIME types to be the default application for";
+          type = types.listOf types.str;
+          default = [
+            "application/illustrator"
+            "application/oxps"
+            "application/pdf"
+            "application/postscript"
+            "application/vnd.comicbook+zip"
+            "application/vnd.comicbook-rar"
+            "application/vnd.ms-xpsdocument"
+            "application/x-bzdvi"
+            "application/x-bzpdf"
+            "application/x-bzpostscript"
+            "application/x-cb7"
+            "application/x-cbr"
+            "application/x-cbt"
+            "application/x-cbz"
+            "application/x-dvi"
+            "application/x-ext-cb7"
+            "application/x-ext-cbr"
+            "application/x-ext-cbt"
+            "application/x-ext-cbz"
+            "application/x-ext-djv"
+            "application/x-ext-djvu"
+            "application/x-ext-dvi"
+            "application/x-ext-eps"
+            "application/x-ext-pdf"
+            "application/x-ext-ps"
+            "application/x-gzdvi"
+            "application/x-gzpdf"
+            "application/x-gzpostscript"
+            "application/x-xzpdf"
+            "image/vnd.djvu+multipage"
+            "image/x-bzeps"
+            "image/x-eps"
+            "image/x-gzeps"
+          ];
+        };
+      };
     };
   };
 
@@ -25,5 +71,9 @@ in
         };
       };
     };
+
+    xdg.mimeApps.defaultApplications = mkIf cfg.defaultApplication.enable (
+      lib.genAttrs cfg.defaultApplication.mimeTypes (_: "zathura.desktop")
+    );
   };
 }
