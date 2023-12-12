299c29585f03ada0bf79f2eb4d72d770d8d8186f
Dave Conroy <dave@tiredofit.ca>
2023-12-12 09:23:29 -0800

    modules/applications/gui/pinta - prepare for mime type additions

--- modules/applications/gui/pinta.nix
+++ modules/applications/gui/pinta.nix
@@ -12,6 +12,20 @@ in
         type = with types; bool;
         description = "Image manipulation";
       };
+      defaultApplication = {
+        enable = mkOption {
+          description = "MIME default application configuration";
+          type = with types; bool;
+          default = false;
+        };
+        mimeTypes = mkOption {
+          description = "MIME types to be the default application for";
+          type = types.listOf types.str;
+          default = [
+
+          ];
+        };
+      };
     };
   };
 
@@ -22,5 +36,8 @@ in
           pinta
         ];
     };
+    xdg.mimeApps.defaultApplications = mkIf cfg.defaultApplication.enable (
+      lib.genAttrs cfg.defaultApplication.mimeTypes (_: "pinta.desktop")
+    );
   };
 }
