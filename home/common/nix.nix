d6484a1cb3da213adf35ab2263759c8fa58a7cc0
Dave Conroy <dave@tiredofit.ca>
2024-03-22 11:18:20 -0700

    home/dommon/nix - refactor nvd log file generation to not output if no changes and order of profiles

--- home/common/nix.nix
+++ home/common/nix.nix
@@ -12,18 +12,30 @@ with lib;
                 PROFILE_PATH=null
             fi
         else
-            PROFILE_PATH="/nix/var/nix/profiles/per-user/$USER"
+            if [ -d /nix/var/nix/profiles/per-user/$USER ] && [ -d $HOME/.local/state/nix/profiles ] ; then
+                PROFILE_PATH="$HOME/.local/state/nix/profiles"
+            else
+                PROFILE_PATH="/nix/var/nix/profiles/per-user/$USER"
+            fi
         fi
 
         if [ -n "$PROFILE_PATH" ] && [ "$PROFILE_PATH" != "null" ]; then
             mkdir -p $HOME/.local/state/home-manager/logs
-            if [ $(ls "$PROFILE_PATH"/profile-*-link 2> /dev/null | wc -l) -gt 0 ]; then
-                ${pkgs.nvd}/bin/nvd diff $(ls -dv $PROFILE_PATH/profile-*-link | tail -2)
-                nvd diff $(ls -dv $PROFILE_PATH/profile-*-link | tail -2) > "$HOME/.local/state/home-manager/logs/$(date +'%Y%m%d%H%M%S')-$USER-$(ls -dv $PROFILE_PATH/profile-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/profile-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log"
-            elif [ $(ls "$PROFILE_PATH"/home-manager-*-link 2> /dev/null | wc -l) -gt 0 ]; then
+            _logfile_now=$(date +'%Y%m%d%H%M%S')
+            if [ $(ls "$PROFILE_PATH"/home-manager-*-link 2> /dev/null | wc -l) -gt 0 ] ; then
                 ${pkgs.nvd}/bin/nvd diff $(ls -dv $PROFILE_PATH/home-manager-*-link | tail -2)
-                nvd diff $(ls -dv $PROFILE_PATH/home-manager-*-link | tail -2) > "$HOME/.local/state/home-manager/logs/$(date +'%Y%m%d%H%M%S')-$USER-$(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log"
+                nvd diff $(ls -dv $PROFILE_PATH/home-manager-*-link | tail -2) > "$HOME/.local/state/home-manager/logs/$_logfile_now-$USER-$(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log"
+                if grep -q "No version or selection state changes" "$HOME/.local/state/home-manager/logs/$_logfile_now-$USER-$(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log" ; then
+                    rm -rf "$HOME/.local/state/home-manager/logs/$_logfile_now-$USER-$(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/home-manager-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log"
+                fi
+            elif [ $(ls "$PROFILE_PATH"/profile-*-link 2> /dev/null | wc -l) -gt 0 ]; then
+                ${pkgs.nvd}/bin/nvd diff $(ls -dv $PROFILE_PATH/profile-*-link | tail -2)
+                nvd diff $(ls -dv $PROFILE_PATH/profile-*-link | tail -2) > "$HOME/.local/state/home-manager/logs/$_logfile_now-$USER-$(ls -dv $PROFILE_PATH/profile-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/profile-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log"
+                if grep -q "No version or selection state changes" "$HOME/.local/state/home-manager/logs/$_logfile_now-$USER-$(ls -dv $PROFILE_PATH/profile-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/profile-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log" ; then
+                    rm -rf "$HOME/.local/state/home-manager/logs/$_logfile_now-$USER-$(ls -dv $PROFILE_PATH/profile-*-link | tail -1 | cut -d '-' -f 3)-$(readlink $(ls -dv $PROFILE_PATH/profile-*-link | tail -1)| cut -d / -f 4 | cut -d - -f 1).log"
+                fi
             fi
+
         else
             echo "ERROR - Can't write Home-Manager generation log file"
         fi
