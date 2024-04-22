37ff1ab79e3afb4bb40db49d654c54c6b13de9d8
Dave Conroy <dave@tiredofit.ca>
2024-04-22 13:58:35 -0700

    modules/desktop/utils/wayland/hyprshot - replace swappy with satty

--- modules/desktop/utils/wayland/hyprshot.nix
+++ modules/desktop/utils/wayland/hyprshot.nix
@@ -20,9 +20,30 @@ in
       packages = with pkgs;
         [
           hyprshot
-          swappy
+          satty
         ];
     };
 
+    xdg.configFile."satty/config.toml".text = ''
+      [general]
+      fullscreen = false
+      early-exit = true
+      initial-tool = "arrow" # [pointer, crop, line, arrow, rectangle, text, marker, blur, brush]
+      copy-command = "wl-copy"
+      output-filename = "/tmp/screenshot-%Y-%m-%d_%H:%M:%S.png"
+      save-after-copy = false
+
+      [font]
+      family = "Roboto"
+      style = "Bold"
+
+      #[color-palette]
+      #first= "#00ffff"
+      #second= "#a52a2a"
+      #third= "#dc143c"
+      #fourth= "#ff1493"
+      #fifth= "#ffd700"
+      #custom= "#008000"
+    '';
   };
 }
