0fb9d3e82c0b83d971f1a2118a754d8a2389f545
Dave Conroy <dave@tiredofit.ca>
2024-05-06 08:48:11 -0700

    Flake - Update hypr input -> 0.40.0

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.39.1";
+      url = "github:hyprwm/Hyprland/v0.40.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland-contrib = {
