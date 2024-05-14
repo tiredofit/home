dfbac71dcda3a455c6a46f160ff63d73973bb0ac
Dave Conroy <dave@tiredofit.ca>
2024-05-14 09:57:34 -0700

    modules/desktop/utils/wayland/hyprpaper - convert to home manager options and implement dynamic configuration building based on displaynumber

--- modules/desktop/utils/wayland/hyprpaper.nix
+++ modules/desktop/utils/wayland/hyprpaper.nix
@@ -1,6 +1,6 @@
-{config, lib, pkgs, ...}:
-
+{ config, inputs, lib, pkgs, specialArgs, ... }:
 let
+  inherit (specialArgs) displays display_center display_left display_right role;
   cfg = config.host.home.applications.hyprpaper;
 in
   with lib;
@@ -20,31 +20,51 @@ in
       file = {
         ".config/hypr/background".source = ../../../../dotfiles/hypr/background;
       };
-
-      packages = with pkgs;
-        [
-          hyprpaper
-        ];
     };
 
-    wayland.windowManager.hyprland = {
+    services = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
+      hyprpaper = {
+        enable = true;
+        settings = {
+          splash = false;
+
+          preload = mkMerge [
+            (mkIf (displays >= 1) (mkAfter [
+              "~/.config/hypr/background/middle.jpg"
+            ]))
+
+            (mkIf (displays >= 2) (mkAfter [
+              "~/.config/hypr/background/right.jpg"
+            ]))
+
+            (mkIf (displays >= 3) (mkAfter [
+              "~/.config/hypr/background/left.jpg"
+            ]))
+          ];
+
+          wallpaper = mkMerge [
+            (mkIf (displays >= 1) (mkAfter [
+              "${display_center},~/.config/hypr/background/middle.jpg"
+            ]))
+
+            (mkIf (displays >= 2) (mkAfter [
+              "${display_right},~/.config/hypr/background/right.jpg"
+            ]))
+
+            (mkIf (displays >= 3) (mkAfter [
+              "${display_left},~/.config/hypr/background/left.jpg"
+            ]))
+          ];
+        };
+      };
+    };
+
+    wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         exec-once = [
           "hyprpaper"
         ];
       };
     };
-
-    ## TODO This should be dynamic based on amount of monitors
-    xdg.configFile."hypr/hyprpaper.conf".text = ''
-      preload = ~/.config/hypr/background/left.jpg
-      preload = ~/.config/hypr/background/middle.jpg
-      preload = ~/.config/hypr/background/right.jpg
-
-      wallpaper = HDMI-A-1,~/.config/hypr/background/right.jpg
-      wallpaper = DP-2,~/.config/hypr/background/middle.jpg
-      wallpaper = DP-3,~/.config/hypr/background/left.jpg
-      splash = false
-    '';
   };
 }
