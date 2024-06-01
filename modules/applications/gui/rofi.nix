42b1ef51555386d6a0b8bf335a09e2e5755595d3
Dave Conroy <dave@tiredofit.ca>
2024-06-01 11:23:43 -0700

    modules/applications/gui/rofi - update theme and config

--- modules/applications/gui/rofi.nix
+++ modules/applications/gui/rofi.nix
@@ -35,27 +35,33 @@ in
       terminal = "${pkgs.kitty}/bin/kitty";
       package = rofiPackage;
       extraConfig = {
-        combi-modi = "window,drun,run";
+        combi-modi = "run,drun";
         cycle = true;
         disable-history = false;
-        display-drun = "";
-        drun-display-format = "{name}";
-        font = "Noto Sans 10";
+        display-Network = " 󰤨  Network";
+        display-drun = "   Apps ";
+        display-run = "   Run ";
+        display-window = " 﩯  window";
+        drun-display-format = "{icon} {name}";
+        font = "Noto Sans 12";
+        hide-scrollbar = true;
         icon-theme = "Papirus";
+        lines = 6;
         modi = "window,drun,run,combi";
         show-icons = true;
         sidebar-mode = true;
         sort = true;
         ssh-client =  "ssh";
       };
-      theme = "~/.config/rofi/themes/top-down.rasi";
+      theme = "~/.config/rofi/themes/toi.rasi";
     };
 
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         bind = [
           "SUPER, R, exec, pkill rofi || ${config.programs.rofi.package}/bin/rofi -run-shell-command '${pkgs.kitty}/bin/kitty' -drun -show run"
-          "SUPER, D, exec, pkill rofi || ${config.programs.rofi.package}/bin/rofi -combi-modi window,drun,ssh,run -show combi -show-icons"
+          "SUPER, D, exec, pkill rofi || ${config.programs.rofi.package}/bin/rofi  -show drun -modi drun -show-icons"
+          #"SUPER, D, exec, pkill rofi || ${config.programs.rofi.package}/bin/rofi -combi-modi window,drun,ssh,run -show combi -show-icons"
           "SUPER, V, exec, ${pkgs.cliphist}'/bin/cliphist list | ${config.programs.rofi.package}/bin/rofi -dmenu | ${pkgs.cliphist}'/bin/cliphist decode | wl-copy"
         ];
         windowrulev2 = [
@@ -172,6 +178,112 @@ in
           border-radius:                  4px;
           border-color:                   @accent;
       }
+    '';
+
+xdg.configFile."rofi/themes/toi.rasi".text = ''
+
+
+* {
+    bg-col:  #1E1D2F;
+    bg-col-light: #1E1D2F;
+    border-col: #1E1D2F;
+    selected-col: #1E1D2F;
+    peach: #FAB387;
+    fg-col: #D9E0EE;
+    fg-col2: #F28FAD;
+    grey: #D9E0EE;
+    width: 600;
+}
+
+element-text, element-icon , mode-switcher {
+    background-color: inherit;
+    text-color:       inherit;
+}
+
+window {
+    height: 360px;
+    width: 30%;
+    border: 3px;
+    border-radius:6px;
+    background-color: @bg-col;
+    border-color:@peach;
+}
+
+mainbox {
+    background-color: @bg-col;
+}
+
+inputbar {
+    children: [prompt,entry];
+    background-color: @bg-col;
+    border-radius: 6px;
+    padding: 2px;
+}
+
+prompt {
+    background-color: @peach;
+    padding: 6px;
+    text-color: @bg-col;
+    border-radius: 6px;
+    margin: 20px 0px 0px 20px;
+}
+
+textbox-prompt-colon {
+    expand: false;
+    str: ":";
+}
+
+entry {
+    padding: 6px;
+    margin: 20px 10px 0px 10px;
+    text-color: @fg-col;
+    background-color: @bg-col;
+    border: 2px;
+    border-radius: 3px;
+    border-color: @peach;
+    placeholder:"Search";
+
+}
+
+listview {
+    border: 0px 0px 0px;
+    padding: 6px 0px 0px;
+    margin: 10px 0px 0px 20px;
+    columns: 2;
+    background-color: @bg-col;
+}
+
+element {
+    padding: 5px;
+    background-color: @bg-col;
+    text-color: @fg-col  ;
+}
+
+element-icon {
+    size: 25px;
+}
+
+element selected {
+    background-color:  @selected-col ;
+    text-color: @fg-col2  ;
+}
+
+mode-switcher {
+    spacing: 0;
+  }
+
+button {
+    padding: 10px;
+    background-color: @bg-col-light;
+    text-color: @grey;
+    vertical-align: 0.5;
+    horizontal-align: 0.5;
+}
+
+button selected {
+  background-color: @bg-col;
+  text-color: @peach;
+}
     '';
   };
 }
--- modules/desktop/windowManager/hyprland/settings.nix
+++ modules/desktop/windowManager/hyprland/settings.nix
@@ -8,6 +8,7 @@ with lib;
   config = mkIf (config.host.home.feature.gui.enable && displayServer == "wayland" && windowManager == "hyprland") {
     wayland.windowManager.hyprland = {
       settings = {
+        source = "../../src/home/dotfiles/hypr/hyprland.conf";
         misc = {
           disable_hyprland_logo = true;
           disable_splash_rendering = true;
