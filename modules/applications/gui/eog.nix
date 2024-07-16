0c48e450542627d789623cc91a5ad1d24457fecf
Dave Conroy <dave@tiredofit.ca>
2024-07-16 14:31:16 -0700

    modules/applications/gui/seahorse|eog|gnomesystemmonitor - Make note for 24.11 to remove gnome prefix

--- modules/applications/gui/eog.nix
+++ modules/applications/gui/eog.nix
@@ -55,7 +55,7 @@ in
     home = {
       packages = with pkgs;
         [
-          gnome.eog
+          gnome.eog # 24.11 - Remove the gnome. prefix
         ];
     };
 
--- modules/applications/gui/seahorse.nix
+++ modules/applications/gui/seahorse.nix
@@ -19,7 +19,7 @@ in
     home = {
       packages = with pkgs;
         [
-          gnome.seahorse
+          gnome.seahorse # 24.11 - Remove the gnome. prefix
         ];
     };
   };
