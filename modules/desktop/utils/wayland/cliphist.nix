c7243eda7dad9b2960453fd77a0b264d330b48ea
Dave Conroy <dave@tiredofit.ca>
2024-10-20 08:30:48 -0700

    modules/desktop/utils/wayland/cliphist - switch back to pkgs

--- modules/desktop/utils/wayland/cliphist.nix
+++ modules/desktop/utils/wayland/cliphist.nix
@@ -19,16 +19,17 @@ in
     home = {
       packages = with pkgs;
         [
-          # 24.11 TODO Remove this overlay
-          (cliphist.overrideAttrs (_old: {
-            src = pkgs.fetchFromGitHub {
-              owner = "sentriz";
-              repo = "cliphist";
-              rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
-              sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
-            };
-            vendorHash = "sha256-M5n7/QWQ5POWE4hSCMa0+GOVhEDCOILYqkSYIGoy/l0=";
-          }))
+          cliphist
+          ## 24.11 TODO Remove this overlay
+          #(cliphist.overrideAttrs (_old: {
+          #  src = pkgs.fetchFromGitHub {
+          #    owner = "sentriz";
+          #    repo = "cliphist";
+          #    rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
+          #    sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
+          #  };
+          #  vendorHash = "sha256-M5n7/QWQ5POWE4hSCMa0+GOVhEDCOILYqkSYIGoy/l0=";
+          #}))
         ];
     };
 
