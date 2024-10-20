e39ceb46e4e53d066054bb0dc4b9ddc1c361807e
Dave Conroy <dave@tiredofit.ca>
2024-10-20 09:00:23 -0700

    modules/applications/gui/florrp - switch to hm module

--- modules/applications/gui/floorp.nix
+++ modules/applications/gui/floorp.nix
@@ -40,11 +40,98 @@ in with lib; {
   };
 
   config = mkIf cfg.enable {
-    home = {
-      packages = with pkgs;
-        [
-          floorp
-        ];
+    programs.floorp = {
+      enable = true;
+      profiles = {
+        dave = mkIf (username == "dave" || username == "media") {
+          name = username;
+          #id = 777;
+          isDefault = true;
+
+          search = {
+            force = true;
+            default = "DuckDuckGo";
+            engines = {
+              "Home Manager Options" = {
+                urls = [{
+                  template =
+                    "https://mipmip.github.io/home-manager-option-search/";
+                  params = [{
+                    name = "query";
+                    value = "{searchTerms}";
+                  }];
+                }];
+                icon =
+                  "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@hmo" ];
+              };
+
+              "Wikipedia (en)".metaData.alias = "@wiki";
+              "Google".metaData.hidden = true;
+            };
+          };
+
+          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+            bitwarden
+            clearurls
+            copy-selected-links
+            copy-selection-as-markdown
+            #decentraleyes
+            enhanced-github
+            facebook-container
+            floccus
+            hover-zoom-plus
+            multi-account-containers
+            image-search-options
+            #localcdn
+            reddit-enhancement-suite
+            sidebery
+            ublock-origin
+            undoclosetabbutton
+            user-agent-string-switcher
+
+            ### Missing:
+            ## Canadian English Dictionary dictionary 3.1.3 true en-CA@dictionaries.addons.mozilla.org
+            ## Download Manager (S3) extension 5.12 true s3download@statusbar
+            ## F.B Purity - Cleans up Facebook extension 36.8.0.0 true fbpElectroWebExt@fbpurity.com
+            ## Hard Refresh Button extension 1.0.0 true {b6da57d3-9727-4bc0-b974-d13e7c004af0}
+            ## Open With extension 7.2.6 true openggwith@darktrojan.net
+            ## PasswordMaker X extension 0.2.2 true passwordmaker@emersion.fr
+            ## Rakuten Canada Button extension 7.8.1 true ebatesca@ebates.com
+            ## StockTrack.ca plugin extension 0.2.4 true {50b98f8c-707d-4dd8-86e4-7c0e15745027}
+            ## The Camelizer extension 3.0.15 true izer@camelcamelcamel.com
+            ## Language: English (CA) locale 114.0.20230608.214645 false langpack-en-CA@firefox.mozilla.org
+
+            # firefox-addons.json | mozilla-addons-to-nix firefox-addons.json output.json
+            #[
+            #  { "slug": "en-CA@dictionaries.addons.mozilla.org" },
+            #  { "slug": "{b6da57d3-9727-4bc0-b974-d13e7c004af0}",
+            #    "pname": "Hard Refresh Button"
+            #  },
+            #  { "slug": "openwith@darktrojan.net" },
+            #  { "slug": "passwordmaker@emersion.fr" },
+            #  { "slug": "ebatesca@ebates.com" },
+            #  { "slug": "{50b98f8c-707d-4dd8-86e4-7c0e15745027}",
+            #    "pname": "StockTrack.ca"
+            #  },
+            #  { "slug": "izer@camelcamelcamel.com" },
+            #  { "slug": "langpack-en-CA@firefox.mozilla.org" },
+            #  { "slug": "s3download@statusbar" }
+            #]
+          ];
+
+          settings = {
+          };
+
+          extraConfig = ''
+          '';
+
+          userChrome = ''
+          '';
+
+          userContent = "\n";
+        };
+      };
     };
 
     xdg.mimeApps.defaultApplications = mkIf cfg.defaultApplication.enable (
