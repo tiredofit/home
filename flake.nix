1027f3f570d3f7586a653eda229a13f5c1c3192e
Dave Conroy <dave@tiredofit.ca>
2024-11-16 14:35:37 -0800

    flake - comment out inputs and overlays not needed right now. Move HM to unstable

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       "nix-command"
     ];
     extra-substituters = [
-      "https://hyprland.cachix.org"
+      #"https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
@@ -16,7 +16,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
   };
@@ -75,10 +75,10 @@
     nix-vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
     };
-    nixpkgs-wayland = {
-      url = "github:nix-community/nixpkgs-wayland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #nixpkgs-wayland = {
+    #  url = "github:nix-community/nixpkgs-wayland";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
     nur = {
       url = "github:nix-community/NUR";
     };
