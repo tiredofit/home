c595621d680301018b5bdc78bff75fa79c08c57a
Dave Conroy <dave@tiredofit.ca>
2024-12-19 10:13:06 -0800

    home/toi/hostname/expedition - add host

---
+++ home/toi/hostname/expedition/default.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, specialArgs, ...}:
+let
+  inherit (specialArgs) role;
+in
+with lib;
+{
+  host = {
+    home = {
+      applications = {
+        act.enable = mkDefault true;
+        hadolint.enable = true;
+        lazydocker.enable = true;
+        lazygit.enable = true;
+        nix-development_tools.enable = true;
+        nmap.enable = mkDefault true;
+        shellcheck.enable = true;
+        shfmt.enable = true;
+        yq.enable = true;
+        yt-dlp.enable = mkDefault true;
+      };
+      user = {
+        dave = {
+          secrets = {
+            act = {
+              toi.enable = true;
+            };
+            github = {
+              toi.enable = true;
+            };
+          };
+        };
+      };
+    };
+  };
+}
