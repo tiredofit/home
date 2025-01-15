2c4578ff7eb96d45c1fd815b8300f6da776d81dc
Dave Conroy <dave@tiredofit.ca>
2025-01-15 07:39:35 -0800

    modules/desktop/utils/agnostic/playerctl - add uwsm prefix to exec

--- modules/desktop/utils/agnostic/playerctl.nix
+++ modules/desktop/utils/agnostic/playerctl.nix
@@ -26,11 +26,11 @@ in
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         bindl = [
-          ",XF86AudioPlay, exec, playerctl play-pause"
-          ",XF86AudioPrev, exec, playerctl previous"
-          ",XF86AudioNext, exec, playerctl next"
-          ",XF86AudioMedia, exec, playerctl play-pause"
-          ",XF86AudioStop, exec, playerctl stop"
+          ",XF86AudioPlay, exec, ${config.host.home.feature.uwsm.prefix}playerctl play-pause"
+          ",XF86AudioPrev, exec, ${config.host.home.feature.uwsm.prefix}playerctl previous"
+          ",XF86AudioNext, exec, ${config.host.home.feature.uwsm.prefix}playerctl next"
+          ",XF86AudioMedia, exec, ${config.host.home.feature.uwsm.prefix}playerctl play-pause"
+          ",XF86AudioStop, exec, ${config.host.home.feature.uwsm.prefix}playerctl stop"
         ];
       };
     };
