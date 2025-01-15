4a88b6878a00bc1035ae8717fdb4240a35908cf5
Dave Conroy <dave@tiredofit.ca>
2025-01-14 23:12:13 -0800

    modules/desktop/utils/wayland/skikane - init

--- modules/desktop/utils/wayland/shikane.nix
+++ modules/desktop/utils/wayland/shikane.nix
@@ -2,15 +2,20 @@
 
 let
   cfg = config.host.home.applications.shikane;
+  tomlFormat = pkgs.formats.toml { };
 in
   with lib;
 {
   options = {
     host.home.applications.shikane = {
       enable = mkOption {
-        default = false;
+        default = true;
         type = with types; bool;
-        description = " Dynamic display configuration";
+        description = "Dynamic output configuration tool";
+      };
+      settings = mkOption {
+        type = tomlFormat.type;
+        default = { };
       };
     };
   };
@@ -23,16 +28,25 @@ in
         ];
     };
 
+    systemd.user.services.shikane = {
+      Unit = {
+        Description = "Dynamic output configuration tool";
+        Documentation = "man:shikane(1)";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.shikane}/bin/shikane -c ${tomlFormat.generate "shikane-config" cfg.settings}";
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+    };
+
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         exec = [
           "systemctl --user restart shikane.service"
-        ];
-        #exec-once = [
-        #  "systemctl --user start shikane.service"
-        #];
-        exec-once = [
-          "shikane"
         ];
       };
     };
