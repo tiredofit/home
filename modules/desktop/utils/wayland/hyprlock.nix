b11989c2d0c93b2744573c1608500041d1ddad16
Dave Conroy <dave@tiredofit.ca>
2025-01-15 07:25:16 -0800

    modules/desktop/utils/wayland/hyprlock - add uwsm prefix to exec

--- modules/desktop/utils/wayland/hyprlock.nix
+++ modules/desktop/utils/wayland/hyprlock.nix
@@ -89,7 +89,7 @@ in
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         bind = [
-          "SUPER_SHIFT, X, exec, hyprlock"
+          "SUPER_SHIFT, X, exec, ${config.host.home.feature.uwsm.prefix}hyprlock"
         ];
       };
     };
