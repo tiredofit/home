5a7755b61fceec31d5d236c4273797f0fbdc7563
Dave Conroy <dave@tiredofit.ca>
2025-01-15 07:32:38 -0800

    modules/applications/gui/nextcloud-client - add uwsm prefix to exec

--- modules/applications/gui/nextcloud-client.nix
+++ modules/applications/gui/nextcloud-client.nix
@@ -26,7 +26,7 @@ in
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         exec-once = [
-          "nextcloud --background"
+          "${config.host.home.feature.uwsm.prefix}nextcloud --background"
         ];
       };
     };
