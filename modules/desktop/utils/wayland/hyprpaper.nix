5ca50397fa81ba59284ec51e32d9109d93bbab09
Dave Conroy <dave@tiredofit.ca>
2025-01-15 07:21:30 -0800

    modules/desktop/utils/wayland/hyprpaper - add uwsm prefix to exec

--- modules/desktop/utils/wayland/hyprpaper.nix
+++ modules/desktop/utils/wayland/hyprpaper.nix
@@ -62,7 +62,7 @@ in
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
       settings = {
         exec-once = [
-          "uwsm app -- hyprpaper"
+          "${config.host.home.feature.uwsm.prefix}hyprpaper"
         ];
       };
     };
