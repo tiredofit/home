45c4f9de112c8a7be75d7f1e27c8508ecd29aa35
Dave Conroy <dave@tiredofit.ca>
2025-01-16 08:04:54 -0800

    modules/desktop/windowManager/hyprland/displays - stop relying on specialArgs

--- modules/desktop/windowManager/hyprland/displays.nix
+++ modules/desktop/windowManager/hyprland/displays.nix
@@ -1,7 +1,5 @@
-{ config, inputs, lib, pkgs, specialArgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
 let
-  inherit (specialArgs) displays display_center display_left display_right hostname role;
-
   displayServer = config.host.home.feature.gui.displayServer ;
   windowManager = config.host.home.feature.gui.windowManager ;
 in
@@ -13,9 +11,9 @@ with lib;
         source = [
           "display.conf"
         ];
-        #monitor = [
-        #  ",preferred,auto,1.0"
-        #];
+        monitor = [
+          ",preferred,auto,1.0"
+        ];
       };
     };
   };
