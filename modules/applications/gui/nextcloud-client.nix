cad66b1ead3b16b2977d27136899cf9aba14481b
Dave Conroy <dave@tiredofit.ca>
2025-01-16 11:31:12 -0800

    modules/applications/nextcloud-client - add hyprland window rules

--- modules/applications/gui/nextcloud-client.nix
+++ modules/applications/gui/nextcloud-client.nix
@@ -28,6 +28,14 @@ in
         exec-once = [
           "${config.host.home.feature.uwsm.prefix}nextcloud --background"
         ];
+        windowrulev2 = [
+          "float,class:(com.nextcloud.desktopclient.nextcloud)"
+          "noborder,class:(com.nextcloud.desktopclient.nextcloud)"
+          "noblur,class:(com.nextcloud.desktopclient.nextcloud)"
+          "noinitialfocus,class:(com.nextcloud.desktopclient.nextcloud)"
+          "move onscreen cursor 50% 0%,class:(com.nextcloud.desktopclient.nextcloud)"
+          "noanim,class:(com.nextcloud.desktopclient.nextcloud)"
+        ];
       };
     };
   };
