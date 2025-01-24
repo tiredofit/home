8e45b2bd187884157703d17bee59c1f242ab1dc2
Dave Conroy <dave@tiredofit.ca>
2025-01-24 08:11:40 -0800

    home/toi/hostname/seed - add applications

--- home/toi/hostname/seed/default.nix
+++ home/toi/hostname/seed/default.nix
@@ -3,9 +3,26 @@
   host = {
     home = {
       applications = {
+        act.enable = true;
+        github-client.enable = true;
+        hadolint.enable = true;
+        lazydocker.enable = true;
+        lazygit.enable = true;
+        nmap.enable = mkDefault true;
+        shellcheck.enable = true;
+        shfmt.enable = true;
       };
-      service = {
-        vscode-server.enable = true;
+      user = {
+        dave = {
+          secrets = {
+            act = {
+              toi.enable = true;
+            };
+            github = {
+              toi.enable = true;
+            };
+          };
+        };
       };
     };
   };
