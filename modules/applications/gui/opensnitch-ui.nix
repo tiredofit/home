41a3347b118cdfa476959ced926b25c99b6bbc1e
Dave Conroy <dave@tiredofit.ca>
2025-07-02 10:38:44 -0700

    modules/applications/gui/opensnitch-ui - update windowrules

--- modules/applications/gui/opensnitch-ui.nix
+++ modules/applications/gui/opensnitch-ui.nix
@@ -22,5 +22,18 @@ in
           opensnitch-ui
         ];
     };
+
+    wayland.windowManager.hyprland = {
+      settings = {
+        windowrule = [
+          "float,initialClass:(^opensnitch_ui$)"
+          "size 900 600,initialClass:(^opensnitch_ui$),initialTitle:(^Preferences$)"
+          "size 900 600,initialClass:(^opensnitch_ui$),initialTitle:(^Rule$)"
+          "size 650 460,initialClass:(^opensnitch_ui$),initialTitle:(Firewall)"
+          "size 650 460,initialClass:(^opensnitch_ui$),initialTitle:(OpenSnitch Network Statistics.*)"
+          "size 650 460,initialClass:(^opensnitch_ui$),initialTitle:(^OpenSnitch v[0-9]\.[0-9]\.[0-9]$)"
+        ];
+      };
+    };
   };
 }
