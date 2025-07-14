55c90304403cefb52e8ceaed02fc1e3864fbda27
Dave Conroy <dave@tiredofit.ca>
2025-07-14 08:03:36 -0700

    pkg - remove wayprompt

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,6 +4,5 @@ pkgs: {
   pkg-cura = pkgs.callPackage ./cura { } ;
   pkg-firefoxpwa = pkgs.callPackage ./firefoxpwa { } ;
   pkg-mqtt-explorer = pkgs.callPackage ./mqtt-explorer { } ;
-  pkg-wayprompt = pkgs.callPackage ./wayprompt { } ;
   pkg-zenbrowser = pkgs.callPackage ./zenbrowser { } ;
 }
