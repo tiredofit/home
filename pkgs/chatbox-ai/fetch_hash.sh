a90c7c9fc1c55fce3d3e5d6706e7ddf565ba72c2
Dave Conroy <dave@tiredofit.ca>
2025-07-14 08:04:26 -0700

    modules/applications/gui/chatbox-ai - remove

--- pkgs/chatbox-ai/fetch_hash.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p curl jq nix gnused
-
-version=$(grep "version = " default.nix | cut -d \" -f2)
-url=$(grep "url = " default.nix | cut -d \" -f2 | sed "s|\${version}|${version}|g")
-hash=$(nix-prefetch-url --type sha256 ${url})
-sed -i "s/hash = \"(.*)\"/hash = \"${hash}\"/g" default.nix
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
 pkgs: {
   pkg-bibata-hyprcursor = pkgs.callPackage ./bibata-hyprcursor { };
-  pkg-chatbox-ai = pkgs.callPackage ./chatbox-ai { } ;
   pkg-cura = pkgs.callPackage ./cura { } ;
   pkg-firefoxpwa = pkgs.callPackage ./firefoxpwa { } ;
   pkg-mqtt-explorer = pkgs.callPackage ./mqtt-explorer { } ;
