60165cd7756c4df2ea4f4a8075e95412017b0d77
Dave Conroy <dave@tiredofit.ca>
2025-08-15 12:35:36 -0700

    modules/applications/cli/docker-compose - update dps alias

--- modules/applications/cli/docker-compose.nix
+++ modules/applications/cli/docker-compose.nix
@@ -72,28 +72,7 @@ with lib;
           }
           alias dip='docker_ip'
 
-          # Enhanced version of 'docker ps' which outputs two extra columns IP and RAM
-          docker_ps() {
-            tmp=$($dsudo $docker_bin_location ps "$@")
-            headings=$(echo "$tmp" | head --lines=1)
-            max_len=$(echo "$tmp" | wc --max-line-length)
-            dps=$(echo "$tmp" | tail --lines=+2)
-            printf "%-''${max_len}s %-15s %10s\n" "$headings" IP RAM
-
-            if [[ -n "$dps" ]]; then
-              while read -r line; do
-                container_short_hash=$(echo "$line" | cut -d' ' -f1)
-                container_long_hash=$($dsudo $docker_bin_location inspect --format="{{.Id}}" "$container_short_hash")
-                container_name=$(echo "$line" | rev | cut -d' ' -f1 | rev)
-                if [ -n "$container_long_hash" ]; then
-                  ram=$(docker_mem "$container_long_hash")
-                  ip=$(docker_ip "$container_name")
-                  printf "%-''${max_len}s %-15s %10s\n" "$line" "$ip" "$ram"
-                fi
-              done <<<"$dps"
-            fi
-          }
-          alias dps='docker_ps'
+          alias dps='$dsudo $docker_bin_location ps --format "table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"'
 
           #  List the volumes for a given container
           docker_vol() {
