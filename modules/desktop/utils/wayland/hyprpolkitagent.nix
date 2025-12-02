4ffa6fc118089ae03bcff22a98eb7f7df4eb48ad
Dave Conroy <dave@tiredofit.ca>
2025-12-02 09:54:48 -0800

    modules/desktop/utils/wayland/hyprpolkitagent - convert to hm options

--- modules/desktop/utils/wayland/hyprpolkitagent.nix
+++ modules/desktop/utils/wayland/hyprpolkitagent.nix
@@ -12,37 +12,13 @@ in
         type = with types; bool;
         description = "Polkit authentication agent written in QT/QML";
       };
-      service.enable = mkOption {
-        default = false;
-        type = with types; bool;
-        description = "Auto start on user session start";
-      };
     };
   };
 
   config = mkIf cfg.enable {
-    home = {
-      packages = with pkgs;
-        [
-          hyprpolkitagent
-        ];
-    };
-
-    systemd.user.services.hyprpolkit = mkIf cfg.service.enable {
-      Unit = {
-        Description = "Hyprland Polkit Authentication Agent";
-        Documentation = "https://github.com/hyprwm/hyprpolkitagent";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
-        ConditionEnvironment = [ "WAYLAND_DISPLAY" ];
-      };
-
-      Service = {
-        ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
-        Slice = "session.slice";
-        TimeoutStopSec = "5sec";
-        Restart = "on-failure";
-      };
+    services.hyprpolkitagent = {
+      enable = mkDefault true;
+      package = mkDefault pkgs.hyprpolkitagent;
     };
   };
 }
