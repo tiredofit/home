43bb946449528a56ee15a0f8b7f6b5a167e161f1
Dave Conroy <dave@tiredofit.ca>
2025-12-02 09:49:23 -0800

    modules/desktop/utils/wayland/hyprpolkitagent - cleanup

--- modules/desktop/utils/wayland/hyprpolkitagent.nix
+++ modules/desktop/utils/wayland/hyprpolkitagent.nix
@@ -44,13 +44,5 @@ in
         Restart = "on-failure";
       };
     };
-
-    #wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
-    #  settings = {
-    #    #exec-once = [
-    #    #  "${config.host.home.feature.uwsm.prefix}systemctl --user start hyprpolkitagent"
-    #    #];
-    #  };
-    #};
   };
 }
