026b03ddb2210f78ae54cc04bf1736a5b0a4cdc4
Dave Conroy <dave@tiredofit.ca>
2025-12-14 10:33:39 -0800

    modules/desktop/utils/wayland/hyprlauncher.nix - init

---
+++ modules/desktop/utils/wayland/hyprlauncher.nix
@@ -0,0 +1,36 @@
+{ config, inputs, lib, pkgs, ... }:
+let
+  cfg = config.host.home.applications.hyprlauncher;
+in
+  with lib;
+{
+  options = {
+    host.home.applications.hyprlauncher = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "HyprLauncher";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs = {
+      hyprlauncher = {
+        enable = true;
+        package = pkgs.hyprlauncher;
+
+        settings = {
+        };
+      };
+    };
+
+    wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland" && config.host.home.feature.gui.enable) {
+      settings = {
+        bind = [
+          "SUPER_SHIFT, T, exec, ${config.host.home.feature.uwsm.prefix}hyprlauncher"
+        ];
+      };
+    };
+  };
+}
