fccd13ebff896b88c7456ded99a4294b4a673867
Dave Conroy <dave@tiredofit.ca>
2026-03-21 08:26:03 -0700

    modules/applications/gui/kitty - support multi shells

--- modules/applications/gui/kitty.nix
+++ modules/applications/gui/kitty.nix
@@ -17,14 +17,53 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    programs = {
-      kitty = {
+  config = mkIf cfg.enable (let
+    shellInit = ''
+      clone() {
+        case "$1" in
+          tab)
+            clone_arg="--type tab"
+          ;;
+          title)
+            clone_arg="--title '$2'"
+          ;;
+          *)
+            clone_arg=$@
+          ;;
+        esac
+
+        clone-in-kitty $clone_arg
+      }
+
+      edit() {
+        case "$2" in
+          tab)
+            edit_arg="--type tab"
+          ;;
+          title)
+            edit_arg="--title '$3'"
+          ;;
+          *)
+            edit_arg="$${@}"
+          ;;
+        esac
+
+        edit-in-kitty $edit_arg
+      }
+
+      if [ -n "$KITTY_WINDOW_ID" ]; then
+        alias ssh="kitty +kitten ssh"
+        alias sssh="/run/current-system/sw/bin/ssh"
+      fi
+    '';
+    in {
+      programs = {
+        kitty = {
         enable = true;
         package = pkgs.unstable.kitty;
-        font = {
-          name = "Hack Nerd Font Mono";
-          size = 11.0;
+        font = mkDefault {
+          name = mkDefault "Hack Nerd Font Mono";
+          size = mkDefault 11.0;
         };
         keybindings = {
           "ctrl+shift+c" = "copy_and_clear_or_interrupt";
@@ -34,32 +73,32 @@ in
         };
         settings = {
           # Font
-          bold_font = "auto";
-          italic_font = "auto";
-          bold_italic_font = "auto";
+          bold_font = mkDefault "auto";
+          italic_font = mkDefault "auto";
+          bold_italic_font = mkDefault "auto";
           ## Cursor
-          cursor_shape = "block";
-          cursor_blink_interval = -1 ;
+          cursor_shape = mkDefault "block";
+          cursor_blink_interval = mkDefault "-1" ;
           ## Scrollback
-          scrollback_lines = 10000;
+          scrollback_lines = mkDefault 10000;
           # Auto Select from Mouse Clipboard;
-          copy_on_select = "clipboard";
-          strip_trailing_spaces = "smart"; # Strip Trailing spaces from Clipboard
-          focus_follows_mouse = "yes";
+          copy_on_select = mkDefault "clipboard";
+          strip_trailing_spaces = mkDefault "smart"; # Strip Trailing spaces from Clipboard
+          focus_follows_mouse = mkDefault "yes";
           ## Bell;
-          enable_audio_bell = "no";
-          visual_bell_duration = "0.2";
-          bell_on_tab  = "'🔔 '";
+          enable_audio_bell = mkDefault "no";
+          visual_bell_duration = mkDefault "0.2";
+          bell_on_tab = mkDefault "'🔔 '";
           # Tab;
-          tab_activity_symbol = "'⚡ '";
-          tab_bar_style = "powerline";
-          tab_powerline_style = "round";
-          tab_bar_min_tabs = 1;
+          tab_activity_symbol = mkDefault "'⚡ '";
+          tab_bar_style = mkDefault "powerline";
+          tab_powerline_style = mkDefault "round";
+          tab_bar_min_tabs = mkDefault 1;
           #tab_title_template "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title}{tab.active_wd}";
           # Tab-bar colors;
           #active_tab_foreground = "#000";
           #active_tab_background = "#eee";
-          active_tab_font_style = "bold-italic";
+          active_tab_font_style = mkDefault "bold-italic";
           #inactive_tab_foreground = "#444";
           #inactive_tab_background = "#999";
           #foreground = "#${config.colorScheme.palette.base05}";
@@ -97,66 +136,34 @@ in
           #color19 = "#${config.colorScheme.palette.base02}";
           #color20 = "#${config.colorScheme.palette.base04}";
           #color21 = "#${config.colorScheme.palette.base06}";
-          inactive_tab_font_style = "normal";
-          confirm_os_window_close = 0;
-          update_check_interval = 0 ; # Disable Updates checking
+          inactive_tab_font_style = mkDefault "normal";
+          confirm_os_window_close = mkDefault 0;
+          update_check_interval = mkDefault 0 ; # Disable Updates checking
           # Performance
-          repaint_delay = 9;
-          input_delay = 2;
-          select_by_word_characters = ":@-./_~?&=%+#" ; # Characters considered a word when double clicking
+          repaint_delay = mkDefault 9;
+          input_delay = mkDefault 2;
+          select_by_word_characters = mkDefault ":@-./_~?&=%+#" ; # Characters considered a word when double clicking
         };
-        shellIntegration.enableBashIntegration = true;
+        shellIntegration = {
+          enableBashIntegration = mkDefault true;
+          enableZshIntegration = mkDefault true;
+        };
       };
 
       bash = {
-        initExtra = ''
-          clone() {
-              case "$1" in
-                  tab)
-                      clone_arg="--type tab"
-                  ;;
-                  title)
-                      clone_arg="--title '$2'"
-                  ;;
-                  *)
-                      clone_arg=$@
-                  ;;
-              esac
-
-              clone-in-kitty $clone_arg
-          }
-
-          edit() {
-              case "$2" in
-                  tab)
-                      edit_arg="--type tab"
-
-                  ;;
-                  title)
-                      edit_arg="--title '$3'"
-                  ;;
-                  *)
-                      edit_arg="$${@}"
-                  ;;
-              esac
-
-              edit-in-kitty $edit_arg
-          }
-
-          if [ -n "$KITTY_WINDOW_ID" ]; then
-              alias ssh="kitty +kitten ssh"
-              alias sssh="/run/current-system/sw/bin/ssh"
-          fi
-        '';
+        initExtra = shellInit;
+      };
+      zsh = {
+        initContent = mkOrder 5000 shellInit;
       };
     };
+
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.enable && displayServer == "wayland" && windowManager == "hyprland") {
       settings = {
-        ## See more in modules/applications/* and modules/desktop/utils/*
         bind = [
           "SUPER, Return, exec, ${config.host.home.feature.uwsm.prefix}${pkgs.kitty}/bin/kitty"
         ];
       };
     };
-  };
+  });
 }
