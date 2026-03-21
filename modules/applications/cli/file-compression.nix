52d6778f104b4592eb9c016d948be44781f00df1
Dave Conroy <dave@tiredofit.ca>
2026-03-21 08:53:49 -0700

    modules/applications/cli/file-compression - support multiple shells

--- modules/applications/cli/file-compression.nix
+++ modules/applications/cli/file-compression.nix
@@ -15,55 +15,39 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    home = {
-      packages = with pkgs;
-        [
-          p7zip
-          pbzip2
-          pigz
-          pixz
-          unrar
-          unzip
-          zip
-          zstd
-        ];
-    };
-
-    programs = {
-      bash = {
-        initExtra = ''
+  config = mkIf cfg.enable (let
+    shellInit = ''
 # Get checksum hash of files
 get_file_hash() {
     if [ -n "$2" ] ; then
         case "$1" in
             md5)
                 md5sum "$2" > "$2".md5
-        ;;
+            ;;
             sha1)
                 sha1sum "$2" > "$2".sha1
-        ;;
+            ;;
             sha256)
                 sha256sum "$2" > "$2".sha256
-        ;;
+            ;;
             sha384)
                 sha384sum "$2" > "$2".sha384
-        ;;
+            ;;
             sha512)
                 sha512sum "$2" > "$2".sha512
-        ;;
+            ;;
             * )
                 :
-        ;;
+            ;;
         esac
     fi
 }
 
-alias hash_md5="get_file_hash md5 "$1""
-alias hash_sha1="get_file_hash sha1 "$1""
-alias hash_sha256="get_file_hash sha256 "$1""
-alias hash_sha384="get_file_hash sha384 "$1""
-alias hash_sha512="get_file_hash sha512 "$1""
+hash_md5()  { get_file_hash md5 "$1"; }
+hash_sha1() { get_file_hash sha1 "$1"; }
+hash_sha256() { get_file_hash sha256 "$1"; }
+hash_sha384() { get_file_hash sha384 "$1"; }
+hash_sha512() { get_file_hash sha512 "$1"; }
 
 # Compress a series of files
 compress () {
@@ -72,52 +56,75 @@ compress () {
       shift
       arg2=$@
       case $arg1 in
-          *.tar.bz2)   tar -I pbzip2 -ccvf $arg1 $arg2 ;;
-          *.tar.gz)    tar -I pigz -cvf $arg1 $arg2 ;;
-          *.tar.xz)    tar -I pixz -cvf $arg1 $arg2 ;;
-          *.tar.zst)   tar cvfa $arg1 $arg2 ;;
-          *.tbz2)      tar -I pbzip2 -cvf $arg1 $arg2 ;;
-          *.tgz)       tar -I pigz -cvf $arg1 $arg2 ;;
-          *.tar)       tar -cvf $arg1 $arg2 ;;
-          *.bz2)       pbzip2 $arg1 ;;
-          *.gz)        pigz $arg1 ;;
-          *.zip)       zip -r $arg1 $arg2 ;;
-          *.Z)         compress $arg1 ;;
-          *.7z)        7z a $arg1 $arg2 ;;
-          *.zst*)      zstd $arg1 ;;
-          *)           echo "don't know how to compress '$1'..." ;;
+          *.tar.bz2)   tar -I pbzip2 -ccvf "$arg1" $arg2 ;;
+          *.tar.gz)    tar -I pigz -cvf "$arg1" $arg2 ;;
+          *.tar.xz)    tar -I pixz -cvf "$arg1" $arg2 ;;
+          *.tar.zst)   tar cvfa "$arg1" $arg2 ;;
+          *.tbz2)      tar -I pbzip2 -cvf "$arg1" $arg2 ;;
+          *.tgz)       tar -I pigz -cvf "$arg1" $arg2 ;;
+          *.tar)       tar -cvf "$arg1" $arg2 ;;
+          *.bz2)       pbzip2 "$arg1" ;;
+          *.gz)        pigz "$arg1" ;;
+          *.zip)       zip -r "$arg1" $arg2 ;;
+          *.Z)         compress "$arg1" ;;
+          *.7z)        7z a "$arg1" $arg2 ;;
+          *.zst*)      zstd "$arg1" ;;
+          *)           echo "don't know how to compress '$arg1'..." ;;
       esac
   else
-    echo "Usage: 'compress <archive_name> <files/path>"
+    echo "Usage: 'compress <archive_name> <files/path>'"
   fi
 }
 
 # Extraction of compressed files
 extract () {
-  if [ -f $1 ] ; then
+  if [ -f "$1" ] ; then
       case $1 in
-          *.tar.bz2)   tar -I pbunzip2 -xvf $1 ;;
-          *.tar.gz)    tar -I pigz -xvf $1 ;;
-          *.tar.xz)    tar -I pixz -xvf $1 ;;
-          *.tar.zst)   tar -xvfa $1 ;;
-          *.tbz2)      tar -I pbzip2 -xvf $1 ;;
-          *.tgz)       tar -I pigz -xvf $1 ;;
-          *.tar)       tar -xvf $1 ;;
-          *.bz2)       pbunzip2 $1 ;;
-          *.gz)        gunzip $1  ;;
-          *.zip)       unzip $1   ;;
-          *.rar)       unrar x $1 ;;
-          *.Z)         uncompress $1 ;;
-          *.7z)        7z x $1    ;;
-          *.zst*)      zstd -d $1 ;;
+          *.tar.bz2)   tar -I pbunzip2 -xvf "$1" ;;
+          *.tar.gz)    tar -I pigz -xvf "$1" ;;
+          *.tar.xz)    tar -I pixz -xvf "$1" ;;
+          *.tar.zst)   tar -xvfa "$1" ;;
+          *.tbz2)      tar -I pbzip2 -xvf "$1" ;;
+          *.tgz)       tar -I pigz -xvf "$1" ;;
+          *.tar)       tar -xvf "$1" ;;
+          *.bz2)       pbunzip2 "$1" ;;
+          *.gz)        gunzip "$1"  ;;
+          *.zip)       unzip "$1"   ;;
+          *.rar)       unrar x "$1" ;;
+          *.Z)         uncompress "$1" ;;
+          *.7z)        7z x "$1"    ;;
+          *.zst*)      zstd -d "$1" ;;
           *)           echo "don't know how to extract '$1'..." ;;
       esac
   else
       echo "extract - '$1' is not a valid file!"
   fi
 }
-        '';
+    '';
+
+    in {
+      home = {
+        packages = with pkgs;
+          [
+            p7zip
+            pbzip2
+            pigz
+            pixz
+            unrar
+            unzip
+            zip
+            zstd
+          ];
       };
-    };
-  };
+
+      programs = {
+        bash = {
+          initExtra = shellInit;
+        };
+
+        zsh = {
+          initContent = shellInit;
+        };
+      };
+    });
 }
