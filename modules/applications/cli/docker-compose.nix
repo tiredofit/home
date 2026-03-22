058967d615b1a1840cec576fae031bd97d5453ff
Dave Conroy <dave@tiredofit.ca>
2026-03-21 18:05:09 -0700

    modules/applications/cli/docker-compose - safe duplicate bash -> zsh temporarily

--- modules/applications/cli/docker-compose.nix
+++ modules/applications/cli/docker-compose.nix
@@ -20,8 +20,10 @@ with lib;
         unstable.docker-compose
       ];
     };
-    programs = (let
-      shellInit = ''
+
+    programs = {
+      bash = {
+        initExtra = ''
           ### Docker
           if [ -n "$XDG_CONFIG_HOME" ] ; then
             export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
@@ -30,10 +32,7 @@ with lib;
           fi
 
           docker_bin_location="$(which docker)"
-          if [ -z "$DOCKER_TIMEOUT" ]; then
-            DOCKER_TIMEOUT="120"
-          fi
-          export DOCKER_TIMEOUT
+          export DOCKER_TIMEOUT=''${DOCKER_TIMEOUT:-"120"}
 
           # Figure out if we need to use sudo for docker commands
           if id -nG "$USER" | grep -qw "docker" || [ $(id -u) = "0" ]; then
@@ -60,7 +59,7 @@ with lib;
                   echo 'n/a'
               fi
           }
-          dmem() { docker_mem "$@"; }
+          alias dmem='docker_mem'
 
           # Get IP Address of a Container
           docker_ip() {
@@ -71,68 +70,38 @@ with lib;
               fi
               echo $ip
           }
-          dip() { docker_ip "$@"; }
+          alias dip='docker_ip'
 
-          dps() { $dsudo $docker_bin_location ps --format "table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"; }
+          alias dps='$dsudo $docker_bin_location ps --format "table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"'
 
           #  List the volumes for a given container
           docker_vol() {
             vols=$($dsudo $docker_bin_location inspect --format="{{.HostConfig.Binds}}" "$1")
-            vols=$(printf "%s" "$vols" | ${pkgs.gnused}/bin/sed 's/^\.\(.*\).$/\1/')
+            vols=''${vols:1:-1}
             for vol in $vols; do
               echo "$vol"
             done
           }
-          dvol() { docker_vol "$@"; }
+          alias dvol='docker_vol'
 
           if command -v "fzf" &>/dev/null; then
             # bash into running container
-            dbash() {
-              c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" |
-                ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |
-                ${pkgs.gawk}/bin/awk '{print $1;}')
-              echo -e "\e[41m**\e[0m Entering $c_name from $(cat /etc/hostname)"
-              $dsudo $docker_bin_location exec -e COLUMNS=$(tput cols) -e LINES=$(tput lines) -it "$c_name" bash
-            }
+            alias dbash='c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gawk}/bin/awk '"'"'{print $1;}'"'"') ; echo -e "\e[41m**\e[0m Entering $c_name from $(cat /etc/hostname)" ; $dsudo $docker_bin_location exec -e COLUMNS=$( tput cols ) -e LINES=$( tput lines ) -it $c_name bash'
 
             # view logs
-            dlog() {
-              c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" |
-                ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |
-                ${pkgs.gnused}/bin/awk '{print $1;}')
-              echo -e "\e[41m**\e[0m Viewing $c_name from $(cat /etc/hostname)"
-              $dsudo $docker_bin_location logs "$c_name" "$1"
-            }
+            alias dlog='c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gnused}/bin/awk '"'"'{print $1;}'"'"') ; echo -e "\e[41m**\e[0m Viewing $c_name from $(cat /etc/hostname)" ; $dsudo $docker_bin_location logs $c_name $1'
 
             # sh into running container
-            dsh() {
-              c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" |
-                ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |
-                ${pkgs.gnused}/bin/awk '{print $1;}')
-              echo -e "\e[41m**\e[0m Entering $c_name from $(cat /etc/hostname)"
-              $dsudo $docker_bin_location exec -e COLUMNS=$(tput cols) -e LINES=$(tput lines) -it "$c_name" sh
-            }
+            alias dsh='c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gnused}/bin/awk '"'"'{print $1;}'"'"') ; echo -e "\e[41m**\e[0m Entering $c_name from $(cat /etc/hostname)" ; $dsudo $docker_bin_location exec -e COLUMNS=$( tput cols ) -e LINES=$( tput lines ) -it $c_name sh'
 
             # Remove running container
-            drm() {
-              target=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" |
-                ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |
-                ${pkgs.gawk}/bin/awk '{print $1;}')
-              $dsudo $docker_bin_location rm "$target"
-            }
+            alias drm='$dsudo $docker_bin_location rm $( $dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gawk}/bin/awk '"'"'{print $1;}'"'"' )'
           fi
 
-          dpull() { $dsudo $docker_bin_location pull "$@"; }
+          alias dpull='$dsudo $docker_bin_location pull'                                                                                                                                                                 # $docker_bin_location Pull
 
           docker_compose_bin_location="$(which docker-compose)"
-          if [ -z "$DOCKER_COMPOSE_TIMEOUT" ]; then
-            if [ -n "$DOCKER_TIMEOUT" ]; then
-              DOCKER_COMPOSE_TIMEOUT="$DOCKER_TIMEOUT"
-            else
-              DOCKER_COMPOSE_TIMEOUT="120"
-            fi
-          fi
-          export DOCKER_COMPOSE_TIMEOUT
+          export DOCKER_COMPOSE_TIMEOUT=''${DOCKER_TIMEOUT:-"120"}
 
           docker-compose() {
            if [ "$2" != "--help" ] ; then
@@ -154,23 +123,143 @@ with lib;
                  $dsudo $docker_compose_bin_location up $arg
                ;;
                * )
-                 $dsudo $docker_compose_bin_location "$@"
+                 $dsudo $docker_compose_bin_location ''${@}
                ;;
             esac
            fi
           }
 
-          dcpull() { $dsudo docker-compose pull "$@"; }                                                                                                 # Docker Compose Pull
-          dcu()    { $dsudo $docker_compose_bin_location up "$@"; }                                                                                        # Docker Compose Up
-          dcud()   { $dsudo $docker_compose_bin_location up -d "$@"; }                                                                                    # Docker Compose Daemonize
-          dcd()    { $dsudo $docker_compose_bin_location down --timeout $DOCKER_COMPOSE_TIMEOUT "$@"; }                                                    # Docker Compose Down
-          dcl()    { $dsudo $docker_compose_bin_location logs -f "$@"; }                                                                                   # Docker Compose Logs
-          dcrecycle() { $dsudo $docker_compose_bin_location down --timeout $DOCKER_COMPOSE_TIMEOUT ; $dsudo $docker_compose_bin_location up -d; }  # Docker Compose Restart
+          alias dcpull='$dsudo docker-compose pull'                                                                                                 # Docker Compose Pull
+          alias dcu='$dsudo $docker_compose_bin_location up'                                                                                        # Docker Compose Up
+          alias dcud='$dsudo $docker_compose_bin_location up -d'                                                                                    # Docker Compose Daemonize
+          alias dcd='$dsudo $docker_compose_bin_location down --timeout $DOCKER_COMPOSE_TIMEOUT'                                                    # Docker Compose Down
+          alias dcl='$dsudo $docker_compose_bin_location logs -f'                                                                                   # Docker Compose Logs
+          alias dcrecycle='$dsudo $docker_compose_bin_location down --timeout $DOCKER_COMPOSE_TIMEOUT ; $dsudo $docker_compose_bin_location up -d'  # Docker Compose Restart
         '';
-      in {
-        bash = { initExtra = shellInit; sessionVariables = { DOCKER_BUILDKIT = 0; }; };
-        zsh = { initContent = shellInit; sessionVariables = { DOCKER_BUILDKIT = 0; }; };
-      }
-    );
+
+        sessionVariables = {
+          DOCKER_BUILDKIT = 0; # Stop using the new buildx
+        };
+      };
+      zsh = {
+        initContent = mkOrder 5000 ''
+          ### Docker
+          if [ -n "$XDG_CONFIG_HOME" ] ; then
+            export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+          else
+            export DOCKER_CONFIG="$HOME/.config/docker"
+          fi
+
+          docker_bin_location="$(which docker)"
+          export DOCKER_TIMEOUT=''${DOCKER_TIMEOUT:-"120"}
+
+          # Figure out if we need to use sudo for docker commands
+          if id -nG "$USER" | grep -qw "docker" || [ $(id -u) = "0" ]; then
+            dsudo=""
+          else
+            dsudo='sudo'
+          fi
+
+          alias dex="$dsudo $docker_bin_location exec -it"                                                           # Execute interactive container, e.g., $dex base /bin/bash
+          alias di="$dsudo $docker_bin_location images"                                                              # Get images
+          alias dki="$dsudo $docker_bin_location run -it -P"                                                         # Run interactive container, e.g., $dki base /bin/bash
+          alias dpsa="$dsudo docker_ps -a"                                                                           # Get process included stop container
+          db() { $dsudo $docker_bin_location build -t="$1" .; }                                                      # Build Docker Image from Current Directory
+          #dri() { $dsudo $docker_bin_location rmi -f $($dsudo $docker_bin_location images -q); }                     # Forcefully Remove all images
+          #drm() { $dsudo $docker_bin_location rm $($dsudo $docker_bin_location ps -a -q); }                          # Remove all containers
+          #drmf() { $dsudo $docker_bin_location stop $($dsudo $docker_bin_location ps -a -q) -timeout $DOCKER_TIMEOUT && $dsudo $docker_bin_location rm $($dsudo $docker_bin_location ps -a -q) ; } # Stop and remove all containers
+          dstop() { $dsudo $docker_bin_location stop $($dsudo $docker_bin_location ps -a -q) -t $DOCKER_TIMEOUT; }   # Stop all containers
+
+          # Get RAM Usage of a Container
+          docker_mem() {
+              if [ -f /sys/fs/cgroup/memory/docker/"$1"/memory.usage_in_bytes ]; then
+                  echo $(($(cat /sys/fs/cgroup/memory/docker/"$1"/memory.usage_in_bytes) / 1024 / 1024)) 'MB'
+              else
+                  echo 'n/a'
+              fi
+          }
+          alias dmem='docker_mem'
+
+          # Get IP Address of a Container
+          docker_ip() {
+              ip=$($dsudo $docker_bin_location inspect --format="{{.NetworkSettings.IPAddress}}" "$1" 2>/dev/null)
+              if (($? >= 1)); then
+                  # Container doesn't exist
+                  ip='n/a'
+              fi
+              echo $ip
+          }
+          alias dip='docker_ip'
+
+          alias dps='$dsudo $docker_bin_location ps --format "table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"'
+
+          #  List the volumes for a given container
+          docker_vol() {
+            vols=$($dsudo $docker_bin_location inspect --format="{{.HostConfig.Binds}}" "$1")
+            vols=''${vols:1:-1}
+            for vol in $vols; do
+              echo "$vol"
+            done
+          }
+          alias dvol='docker_vol'
+
+          if command -v "fzf" &>/dev/null; then
+            # bash into running container
+            alias dbash='c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gawk}/bin/awk '"'"'{print $1;}'"'"') ; echo -e "\e[41m**\e[0m Entering $c_name from $(cat /etc/hostname)" ; $dsudo $docker_bin_location exec -e COLUMNS=$( tput cols ) -e LINES=$( tput lines ) -it $c_name bash'
+
+            # view logs
+            alias dlog='c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gnused}/bin/awk '"'"'{print $1;}'"'"') ; echo -e "\e[41m**\e[0m Viewing $c_name from $(cat /etc/hostname)" ; $dsudo $docker_bin_location logs $c_name $1'
+
+            # sh into running container
+            alias dsh='c_name=$($dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gnused}/bin/awk '"'"'{print $1;}'"'"') ; echo -e "\e[41m**\e[0m Entering $c_name from $(cat /etc/hostname)" ; $dsudo $docker_bin_location exec -e COLUMNS=$( tput cols ) -e LINES=$( tput lines ) -it $c_name sh'
+
+            # Remove running container
+            alias drm='$dsudo $docker_bin_location rm $( $dsudo $docker_bin_location ps --format "table {{.Names}}\t{{.Image}}\t{{ .ID}}\t{{.RunningFor}}" | ${pkgs.gnused}/bin/sed "/NAMES/d" | sort | fzf --tac |  ${pkgs.gawk}/bin/awk '"'"'{print $1;}'"'"' )'
+          fi
+
+          alias dpull='$dsudo $docker_bin_location pull'                                                                                                                                                                 # $docker_bin_location Pull
+
+          docker_compose_bin_location="$(which docker-compose)"
+          export DOCKER_COMPOSE_TIMEOUT=''${DOCKER_TIMEOUT:-"120"}
+
+          docker-compose() {
+           if [ "$2" != "--help" ] ; then
+             case "$1" in
+               "down" )
+                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+                 $dsudo $docker_compose_bin_location down --timeout $DOCKER_COMPOSE_TIMEOUT $arg
+               ;;
+               "restart" )
+                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+                 $dsudo $docker_compose_bin_location restart --timeout $DOCKER_COMPOSE_TIMEOUT $arg
+               ;;
+               "stop" )
+                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+                 $dsudo $docker_compose_bin_location stop --timeout $DOCKER_COMPOSE_TIMEOUT $arg
+               ;;
+               "up" )
+                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+                 $dsudo $docker_compose_bin_location up $arg
+               ;;
+               * )
+                 $dsudo $docker_compose_bin_location ''${@}
+               ;;
+            esac
+           fi
+          }
+
+          alias dcpull='$dsudo docker-compose pull'                                                                                                 # Docker Compose Pull
+          alias dcu='$dsudo $docker_compose_bin_location up'                                                                                        # Docker Compose Up
+          alias dcud='$dsudo $docker_compose_bin_location up -d'                                                                                    # Docker Compose Daemonize
+          alias dcd='$dsudo $docker_compose_bin_location down --timeout $DOCKER_COMPOSE_TIMEOUT'                                                    # Docker Compose Down
+          alias dcl='$dsudo $docker_compose_bin_location logs -f'                                                                                   # Docker Compose Logs
+          alias dcrecycle='$dsudo $docker_compose_bin_location down --timeout $DOCKER_COMPOSE_TIMEOUT ; $dsudo $docker_compose_bin_location up -d'  # Docker Compose Restart
+        '';
+
+        sessionVariables = {
+          DOCKER_BUILDKIT = 0; # Stop using the new buildx
+        };
+      };
+    };
   };
 }
