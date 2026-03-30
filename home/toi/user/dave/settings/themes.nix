dd6eab1df6eb1a9cf5dba42972f72afc4a2522c3
Dave Conroy <dave@tiredofit.ca>
2026-03-30 10:23:43 -0700

    home/toi/user/dave/settings/themes - switch theme for gtk 4 support

--- home/toi/user/dave/settings/themes.nix
+++ home/toi/user/dave/settings/themes.nix
@@ -12,28 +12,44 @@ in
   config = mkIf config.host.home.feature.theming.enable {
     #colorScheme = inputs.nix-colors.colorSchemes.dracula;
 
-    gtk = mkIf ((role == "workstation" || role == "laptop")) {
-      enable = mkDefault true;
-      iconTheme = {
-        name = "Papirus";
-        package = pkgs.papirus-icon-theme;
-        #name = "BeautyLine";
-        #package = pkgs.beauty-line-icon-theme;
-      };
-      theme = {
-        name = "Zukitwo";
-        package = pkgs.zuki-themes;
+  gtk = mkIf ((role == "workstation" || role == "laptop")) {
+    enable = mkDefault true;
+    gtk3 = {
+      extraConfig = {
+        gtk-application-prefer-dark-theme = mkDefault 1;
       };
     };
+    gtk4 = {
+      extraConfig = {
+        gtk-application-prefer-dark-theme = mkDefault 1;
+      };
+      theme = mkDefault config.gtk.theme;
+    };
+
+    iconTheme = {
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    };
+
     #pointerCursor =  {
     #  gtk.enable = true;
     #  name = "Quintom_Snow";
     #  package = pkgs.quintom-cursor-theme;
     #};
 
+    theme = {
+      name = "Catppuccin-Mocha";
+      package = pkgs.catppuccin-gtk.override {
+        tweaks = [ "rimless" ];
+        variant = "mocha";
+      };
+    };
+  };
+
    programs = mkIf ((role == "workstation" || role == "laptop")) (let
       sessionVars = {
         GTK2_RC_FILES = "$XDG_CONFIG_HOME/gtk-2.0/gtkrc";
+        GTK_THEME = "Catppuccin-Mocha";
       };
     in {
       bash = {
