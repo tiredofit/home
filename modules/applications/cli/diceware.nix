7dbd4d33fcdb3ae3b07812106524d32c5039ce07
Dave Conroy <dave@tiredofit.ca>
2026-04-10 10:01:15 -0700

    modules/applications/cli/diceware - fix some bash only conversions

--- modules/applications/cli/diceware.nix
+++ modules/applications/cli/diceware.nix
@@ -17,33 +17,33 @@ in
 
   config = mkIf cfg.enable (let pwgenScript = ''
 
-      pwgen() {
-        ## pwgen <number of words>
-        local genpassword
-        local pwords
-        local counter
-        local s_password
-        if [ -n "$1" ] ; then pwords=$1 ; else pwords=4 ; fi
-        counter=1
-        for s_password in $(eval echo "{1..$pwords}") ; do
+    pwgen() {
+      ## pwgen <number of words>
+      local genpassword
+      local pwords
+      local counter
+      local s_password
+      if [ -n "$1" ] ; then pwords=$1 ; else pwords=4 ; fi
+      counter=1
+      for s_password in $(eval echo "{1..$pwords}") ; do
         case $counter in
-                "2" | "4" | "6" | "8" | "10" | "12" | "14" | "16" )
-                        s_password=$(diceware -n 1)
-                        s_password=''${s_password^^}
-                ;;
-                "1" | "3" | "5" | "7" | "9" | "11" | "13" | "15" )
-                        s_password=$(diceware -n 1 --no-caps)
-                        s_password=''${s_password,,}
-                ;;
+          "2" | "4" | "6" | "8" | "10" | "12" | "14" | "16" )
+            s_password=$(diceware -n 1)
+            s_password=$(printf "%s" "$s_password" | tr '[:lower:]' '[:upper:]')
+          ;;
+          "1" | "3" | "5" | "7" | "9" | "11" | "13" | "15" )
+            s_password=$(diceware -n 1 --no-caps)
+            s_password=$(printf "%s" "$s_password" | tr '[:upper:]' '[:lower:]')
+            ;;
         esac
         if [ "$counter" -lt $pwords ] ; then
           s_password="$s_password-"
         fi
         genpassword=$genpassword$s_password
         (( counter+=1 ))
-        done
-        echo $genpassword
-      }
+      done
+      echo $genpassword
+    }
     '';
     in {
       home = {
