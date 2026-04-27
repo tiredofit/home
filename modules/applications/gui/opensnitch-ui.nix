c0e811b54b094951f8df12f1b4c58bf75c6c7fb8
Dave Conroy <dave@tiredofit.ca>
2026-04-26 20:27:10 -0700

    modules/applicaitons/gui/opensnitch - conditionally add hyprland config

--- modules/applications/gui/opensnitch-ui.nix
+++ modules/applications/gui/opensnitch-ui.nix
@@ -23,7 +23,7 @@ in
       };
     };
 
-    wayland.windowManager.hyprland = {
+    wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.enable && config.host.home.feature.gui.displayServer == "wayland" && config.host.home.feature.gui.windowManager == "hyprland") {
       settings = {
         windowrule = [
           "float,initialClass:(^opensnitch_ui$)"
