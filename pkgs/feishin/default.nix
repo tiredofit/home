775a0a9d4f8fd194ed8bf9b346143ee03df6b3a3
Dave Conroy <dave@tiredofit.ca>
2026-04-27 04:07:37 -0700

    pkgs/feishin - bump to 1.11.0

--- pkgs/feishin/default.nix
+++ pkgs/feishin/default.nix
@@ -7,7 +7,7 @@
 
 appimageTools.wrapType2 rec {
   pname = "feishin";
-  version = "v1.9.0";
+  version = "v1.11.0";
 
   extraPkgs = pkgs: [
     pkgs.gnome-keyring
@@ -16,7 +16,7 @@ appimageTools.wrapType2 rec {
 
   src = fetchurl {
     url = "https://github.com/jeffvli/feishin/releases/download/${version}/Feishin-linux-x86_64.AppImage";
-    sha256 = "0gy1pa75lcff4n9awfvvx81aw4npvmlc65jhn2047yqyw64vgdn4";
+    sha256 = "12havkv29d983c5wsx9px9jdgs4rn6a95gqls3amf55sgn48afw4";
   };
 
   desktopItems = [
