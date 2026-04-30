cb583160a73d9c68553b6414f8f02b0795fa6b9f
Dave Conroy <dave@tiredofit.ca>
2026-04-30 09:08:15 -0700

    modules/desktop/shell/dms - setup more scaffolding for declarative config

--- modules/desktop/shell/dms/default.nix
+++ modules/desktop/shell/dms/default.nix
@@ -13,13 +13,14 @@ with lib;
     inputs.dms.homeModules.dank-material-shell
     inputs.dms.homeModules.niri
     inputs.danksearch.homeModules.dsearch
+    inputs.dms-plugin-registry.modules.default
   ];
 
   config = mkIf dmsActive {
     programs = {
       dank-material-shell = {
         enable = true;
-        systemd= {
+        systemd = {
           enable = mkDefault true;
           restartIfChanged = mkDefault true;
         };
@@ -36,6 +37,10 @@ with lib;
           enableSpawn = mkDefault false;
         };
 
+        #plugins = {
+          #dankBatteryAlerts.enable = true;
+          #dankBatteryAlerts.src = inputs.dms-plugin-registry.packages.${pkgs.system}.dankBatteryAlerts;
+        #};
         #settings = { # Enable to allow it to export JSON settings https://stevebinary.github.io/json2nix/
         #  theme = "dark";
         #  dynamicTheming = true;
@@ -53,7 +58,7 @@ with lib;
           worker_count = mkDefault 4;
           index_all_files = mkDefault true;
 
-          auto_reindex = mkDefault false;
+          auto_reindex = mkDefault true;
           reindex_interval_hours = mkDefault 24;
 
           # Text file extensions
@@ -72,7 +77,7 @@ with lib;
               exclude_dirs = [ "node_modules" "venv" "target" ];
             }
             {
-              path = "~/Projects";
+              path = "~/src";
               max_depth = 8;
               exclude_hidden = true;
               exclude_dirs = [ "node_modules" ".git" "target" "dist" ];
@@ -95,6 +100,70 @@ with lib;
       #niri-flake-polkit = mkIf niriActive {
       #  Install.WantedBy = mkForce [];
       #};
+    };
+
+    wayland.windowManager.hyprland = mkIf (dmsActive && config.programs.dank-material-shell.systemd.enable) {
+      settings = {
+        # === Application Launchers ===
+        bind = [
+          "SUPER, space, exec, dms ipc call spotlight toggle"
+          "SUPER, V, exec, dms ipc call clipboard toggle"
+          "SUPER, M, exec, dms ipc call processlist focusOrToggle"
+          "SUPER, comma, exec, dms ipc call settings focusOrToggle"
+          "SUPER, N, exec, dms ipc call notifications toggle"
+          "SUPER SHIFT, N, exec, dms ipc call notepad toggle"
+          #"SUPER, Y, exec, dms ipc call dankdash wallpaper"
+          "SUPER, TAB, exec, dms ipc call hypr toggleOverview"
+          "SUPER, P, exec, dms ipc call powermenu toggle"
+          # === Cheat sheet ===
+          "SUPER SHIFT, Slash, exec, dms ipc call keybinds toggle hyprland"
+          # === Security ===
+          "SUPER SHIFT, X, exec, dms ipc call lock lock"
+          "CTRL ALT, Delete, exec, dms ipc call processlist focusOrToggle"
+
+          # === Screenshots ===
+          #", Print, exec, dms screenshot"
+          #"CTRL, Print, exec, dms screenshot full"
+          #"ALT, Print, exec, dms screenshot window"
+
+          #"SUPER SHIFT, S, exec, dms screenshot --no-file --reset"
+        ];
+
+        bindel = [
+          # === Audio Controls ===
+          ", XF86AudioRaiseVolume, exec, dms ipc call audio increment 1"
+          ", XF86AudioLowerVolume, exec, dms ipc call audio decrement 1"
+          "CTRL, XF86AudioRaiseVolume, exec, dms ipc call mpris increment 1"
+          "CTRL, XF86AudioLowerVolume, exec, dms ipc call mpris decrement 1"
+          # === Brightness Controls ===
+          ", XF86MonBrightnessUp, exec, dms ipc call brightness increment 5 \"\""
+          ", XF86MonBrightnessDown, exec, dms ipc call brightness decrement 5 \"\""
+        ];
+        bindl = [
+          # === Audio Controls ===
+          ", XF86AudioMute, exec, dms ipc call audio mute"
+          ", XF86AudioMicMute, exec, dms ipc call audio micmute"
+          ", XF86AudioPause, exec, dms ipc call mpris playPause"
+          ", XF86AudioPlay, exec, dms ipc call mpris playPause"
+          ", XF86AudioPrev, exec, dms ipc call mpris previous"
+          ", XF86AudioNext, exec, dms ipc call mpris next"
+        ];
+        source = [
+          #"./dms/binds.conf"
+          "./dms/colors.conf"
+          "./dms/cursor.conf"
+          "./dms/layout.conf"
+          "./dms/outputs.conf"
+          "./dms/windowrules.conf"
+        ];
+        layerrule = [
+          "no_anim on, match:namespace ^dms:.*"
+          "no_anim on, match:namespace ^(quickshell)$"
+        ];
+        windowrule = [
+          "float on, match:class ^(org.quickshell)$"
+        ];
+      };
     };
   };
 }
