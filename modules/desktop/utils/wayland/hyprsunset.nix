3476b7364c662303af1b52f736e413266b3e1653
Dave Conroy <dave@tiredofit.ca>
2026-04-29 22:02:38 -0700

    modules/desktop/utils/wayland/hyprsunset - add service gate and package backup

--- modules/desktop/utils/wayland/hyprsunset.nix
+++ modules/desktop/utils/wayland/hyprsunset.nix
@@ -12,11 +12,19 @@ in
         type = with types; bool;
         description = "Application to enable a blue-light filter on Hyprland";
       };
+      service.enable = mkOption {
+        default = true;
+        type = with types; bool;
+        description = "Auto start on user session start";
+      };
     };
   };
 
   config = mkIf cfg.enable {
-    services.hyprsunset = {
+    home.packages = with pkgs; [
+      hyprsunset
+    ];
+    services.hyprsunset = mkIf cfg.service.enable {
       enable = mkDefault true;
       package = mkDefault pkgs.hyprsunset;
     };
