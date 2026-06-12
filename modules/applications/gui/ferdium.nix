9b279a165c3aa48bd240a5a5fa0be2498eb9c5b5
Dave Conroy <dave@tiredofit.ca>
2026-06-12 11:31:48 -0700

    modules/applications/gui/ferdium - add service functionalith and fix hypr startup

--- modules/applications/gui/ferdium.nix
+++ modules/applications/gui/ferdium.nix
@@ -2,6 +2,14 @@
 
 let
   cfg = config.host.home.applications.ferdium;
+  flags = [
+    "--enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer"
+    "--ozone-platform=wayland"
+    "--enable-smooth-scrolling"
+  ];
+  ferdium-wrapped = pkgs.writeShellScriptBin "ferdium" ''
+    exec ${pkgs.unstable.ferdium}/bin/ferdium ${builtins.toString flags} "$@"
+  '';
 in
   with lib;
 {
@@ -12,20 +20,42 @@ in
         type = with types; bool;
         description = "Multi Messaging tool";
       };
+      service = {
+        enable = mkOption {
+          default = false;
+          type = with types; bool;
+          description = "Enable to start as a systemd user service";
+        };
+      };
     };
   };
 
   config = mkIf cfg.enable {
     home = {
-      packages = with pkgs;
-        [
-          unstable.ferdium
-        ];
+      packages = with pkgs; [
+        ferdium-wrapped
+      ];
+    };
+
+    systemd.user.services.ferdium = mkIf cfg.service.enable {
+      Unit = {
+        Description = "Ferdium - Multi Messaging Tool";
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${ferdium-wrapped}/bin/ferdium";
+        Restart = "on-failure";
+        RestartSec = "3";
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
     };
 
     wayland.windowManager.hyprland = mkIf (config.host.home.feature.gui.displayServer == "wayland" && builtins.elem "hyprland" config.host.home.feature.gui.windowManager && config.host.home.feature.gui.enable) {
       settings = {
-        on._args = ["hyprland.start" (lib.generators.mkLuaInline "function() hl.exec_cmd('${config.host.home.feature.uwsm.prefix}ferdium --ozone-platform=wayland --enable-features-WaylandWindowDecorations') end")];
+        on = mkIf (!cfg.service.enable) [{ _args = ["hyprland.start" (lib.generators.mkLuaInline "function() hl.exec_cmd('${config.host.home.feature.uwsm.prefix}ferdium') end")]; }];
         window_rule = [
           {
             workspace = "3";
