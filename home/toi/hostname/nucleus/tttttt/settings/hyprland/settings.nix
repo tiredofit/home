d1ee055aa7f389aff235de2324afc48ed8b2ea12
Dave Conroy <dave@tiredofit.ca>
2026-06-12 11:34:32 -0700

    home/toi/hostname/nucleus - fix hypr lua startup

--- home/toi/hostname/nucleus/tttttt/settings/hyprland/settings.nix
+++ home/toi/hostname/nucleus/tttttt/settings/hyprland/settings.nix
@@ -8,7 +8,7 @@ with lib;
   config = mkIf (config.host.home.feature.gui.enable && displayServer == "wayland" && builtins.elem "hyprland" windowManager) {
     wayland.windowManager.hyprland = {
       settings = {
-        on._args = ["hyprland.start" (lib.generators.mkLuaInline "function() hl.exec_cmd('${config.host.home.feature.uwsm.prefix}virt-manager') hl.exec_cmd('${config.host.home.feature.uwsm.prefix}ghosttyu') hl.exec_cmd('${config.host.home.feature.uwsm.prefix}firefox') end")];
+        on = [{ _args = ["hyprland.start" (lib.generators.mkLuaInline "function() hl.exec_cmd('${config.host.home.feature.uwsm.prefix}virt-manager') hl.exec_cmd('${config.host.home.feature.uwsm.prefix}ghosttyu') hl.exec_cmd('${config.host.home.feature.uwsm.prefix}firefox') end")]; }];
       };
     };
   };
