61bb4b344c9766bca7efec94366cdb5e01fb53b3
Dave Conroy <dave@tiredofit.ca>
2026-06-12 11:32:06 -0700

    modules/applications/gui/networkmanager - fix hypr lua startup

--- modules/applications/gui/networkmanager.nix
+++ modules/applications/gui/networkmanager.nix
@@ -27,7 +27,7 @@ in
 
     wayland.windowManager.hyprland = mkIf ((cfg.systemtray.enable) && (config.host.home.feature.gui.displayServer == "wayland" && builtins.elem "hyprland" config.host.home.feature.gui.windowManager && config.host.home.feature.gui.enable)) {
       settings = {
-        on._args = ["hyprland.start" (lib.generators.mkLuaInline "function() hl.exec_cmd('${config.host.home.feature.uwsm.prefix}nm-applet') end")];
+        on = [{ _args = ["hyprland.start" (lib.generators.mkLuaInline "function() hl.exec_cmd('${config.host.home.feature.uwsm.prefix}nm-applet') end")]; }];
         window_rule = [
           {
             float = true;
