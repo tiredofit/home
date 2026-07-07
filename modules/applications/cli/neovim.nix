32b93a2396cb1490b7230d54d1678f618f6e3c29
Dave Conroy <dave@tiredofit.ca>
2026-07-07 10:29:03 -0700

    modules/applicationc/cli/neovim - optionize

--- modules/applications/cli/neovim.nix
+++ modules/applications/cli/neovim.nix
@@ -16,6 +16,100 @@ in
         type = with types; bool;
         description = "Editor";
       };
+      lsp = {
+        bash = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Bash LSP (bashls)";
+        };
+        docker = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Docker LSP servers";
+        };
+        dotnet = mkOption {
+          default = true;
+          type = with types; bool;
+          description = ".NET LSP (dotls)";
+        };
+        go = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Go LSP (gopls)";
+        };
+        html = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "HTML LSP servers (superhtml, html)";
+        };
+        jq = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "jq LSP (jqls)";
+        };
+        json = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "JSON LSP (jsonls)";
+        };
+        lua = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Lua LSP (lua_ls)";
+        };
+        markdown = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Markdown LSP (marksman)";
+        };
+        nix = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Nix LSP (nil_ls)";
+        };
+        phpactor = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "PHP LSP (phpactor)";
+        };
+        python = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Python LSP (pyright)";
+        };
+        sql = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "SQL LSP (sqls)";
+        };
+        tailwind = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Tailwind CSS LSP";
+        };
+        typescript = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "TypeScript/JavaScript LSP (ts_ls)";
+        };
+        yaml = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "YAML LSP (yamlls)";
+        };
+      };
+      treesitter = {
+        nixGrammars = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Install tree-sitter grammars via nixpkgs";
+        };
+      };
+      formatters = mkOption {
+        default = true;
+        type = with types; bool;
+        description = "Install formatters (prettier, stylua)";
+      };
     };
   };
 
@@ -26,9 +120,7 @@ in
   in {
     home = {
       packages = with pkgs;
-        [
-          prettier
-        ];
+        optional cfg.formatters prettier;
     };
     
     host.home.applications = {
