4e36de50bb3a18faf942e754b7300e7d8b60c075
Dave Conroy <dave@tiredofit.ca>
2026-07-21 23:55:02 -0700

    home/toi/hostname/enigma - config

--- home/toi/hostname/enigma/default.nix
+++ home/toi/hostname/enigma/default.nix
@@ -3,30 +3,11 @@
   host = {
     home = {
       applications = {
-        beets = {
-          enable = true;
-          plugins.bandcamp = true;
-        };
         direnv.enable = true;
         mcp-servers = {
           enable = true;
           secretsFile = ../../user/dave/secrets/mcp/mcp.yaml;
           servers = {
-            context7.enable = true;
-            everything.enable = true;
-            fetch.enable = true;
-            filesystem = {
-              enable = true;
-              args = [ "/home/dave/src/" ];
-            };
-            git = {
-              enable = true;
-              args = [ "--repository" "/home/dave/src/nfra/" ];
-            };
-            github = {
-              enable = true;
-              secretEnv = { GITHUB_PERSONAL_ACCESS_TOKEN = "mcp/github_token"; };
-            };
             homeassistant = {
               enable = true;
               secretEnv = {
@@ -34,21 +15,9 @@
                 HOMEASSISTANT_TOKEN = "mcp/homeassistant_token";
               };
             };
-            mcp-nixos.enable = true;
-            memory.enable = true;
-            mqtt = {
-              enable = true;
-              #secretEnv = {
-              #  MQTT_HOST = "mcp/mqtt_host";
-              #  MQTT_PORT = "mcp/mqtt_port";
-              #  MQTT_USERNAME = "mcp/mqtt_username";
-              #  MQTT_PASSWORD = "mcp/mqtt_password";
-              #};
-              #env = {
-              #  MQTT_HOST  = "192.168.1.1";
-              #  MQTT_PORT  = "1883";
-              #};
-            };
+            opensense.enable = true;
+            zigbee2mqtt.enable = true;
+
           };
         };
         mp3gain.enable = true;
